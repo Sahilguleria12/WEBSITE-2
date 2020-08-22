aadf9c97478b8155c4bb8665736d32028e359bb4
Sahil Guleria <68143032+Sahil-Guleria@users.noreply.github.com>
2020-08-22 15:25:10 +0530

    Update database.js

--- database.js
+++ database.js
@@ -1,8 +1,8 @@
 const mysql = require('mysql2');
 const dbConnection = mysql.createPool({
     host     : 'localhost', // MYSQL HOST NAME
-    user     : 'root', // MYSQL USERNAME
-    password : 'Sahil!@#123', // MYSQL PASSWORD
-    database : 'nodejs_login_register' // MYSQL DB NAME
+    user     : '', // MYSQL USERNAME
+    password : '', // MYSQL PASSWORD
+    database : '' // MYSQL DB NAME
 }).promise();
 module.exports = dbConnection;
